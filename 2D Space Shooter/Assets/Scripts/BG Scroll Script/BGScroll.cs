ac7410f0d6756359b034063105e0c5bd5a4e1b67
furkanbass <furkanbas0626@gmail.com>
2022-04-22 03:51:11 -0400

    Second commit

---
+++ 2D Space Shooter/Assets/Scripts/BG Scroll Script/BGScroll.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour {
+
+    public float scroll_Speed = 0.1f;
+
+    private MeshRenderer mesh_Renderer;
+
+    private float y_Scroll;
+
+    void Awake() {
+        mesh_Renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Scroll();
+    }
+
+    void Scroll() {
+
+        y_Scroll = Time.time * scroll_Speed;
+
+        Vector2 offset = new Vector2(y_Scroll, 0f);
+        mesh_Renderer.sharedMaterial.SetTextureOffset("_MainText", offset);
+
+    }
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
